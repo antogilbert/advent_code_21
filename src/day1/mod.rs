86d01c9ee9d44ed2b486faf54437eabd04c1d8d4
Antonello Palazzi <antonello.palazzi@gmail.com>
2021-12-04 18:34:02 +0000

    Day 1 - Part 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -26,13 +26,26 @@ impl Runnable for Day1 {
     fn run(&self) {
         let mut increments = 0;
         let mut previous = i32::MAX;
-        for i in self.read() {
-            if i > previous {
+        let v = self.read();
+        for i in &v {
+            if i > &previous {
                 increments += 1;
             }
-            previous = i;
+            previous = i.clone();
         }
 
-        println!("Day1 - Increments: {}", increments);
+        println!("Day1 Part 1- Increments: {}", increments);
+
+        let mut sliding_inc = 0;
+        let mut sliding_prev = v[0] + v[1] + v[2];
+
+        for i in 3..v.len() {
+            let new_val = sliding_prev + v[i] - v[i-3];
+            if new_val > sliding_prev {
+                sliding_inc += 1;
+            }
+            sliding_prev = new_val;
+        }
+        println!("Day1 Part 2 - Window Increments: {}", sliding_inc);
     }
 }

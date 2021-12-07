5c8f6267d60b63e61a9adc2b1c0fbab940ef12b4
Antonello Palazzi <antonello.palazzi@gmail.com>
2021-12-07 22:30:28 +0000

    Day 4 - Part 2

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -6,17 +6,27 @@ pub struct Day4 {
     file: String,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Ord)]
 struct Board {
     board: Vec<Vec<i32>>,
-    pub is_bingo: bool
+    pub is_bingo: bool,
+    pub win_turn: u32,
+    pub win_val: i32
+}
+
+impl PartialOrd for Board {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.win_turn.cmp(&other.win_turn))
+    }
 }
 
 impl Board {
     fn new() -> Self {
         Board {
             board: Vec::new(),
-            is_bingo: false
+            is_bingo: false,
+            win_turn: u32::MAX,
+            win_val: 0
         }
     }    
     
@@ -37,7 +47,7 @@ impl Board {
         }
     }
 
-    pub fn check_bingo(&mut self) -> bool {
+    pub fn check_bingo(&mut self) {
         let mut bingo = true;
         for row in &self.board {
             bingo = true;
@@ -46,30 +56,30 @@ impl Board {
             }
             if bingo {
                 self.is_bingo = bingo;
-                return bingo;
+                return;
             }
         }
 
         for i in 0..self.board.len() {
             bingo = true;
             for j in 0..self.board.len() {
-                bingo = bingo && (self.board[i][j] < 0)
+                bingo = bingo && (self.board[j][i] < 0)
             }
             if bingo {
                 self.is_bingo = bingo;
-                return bingo;
+                return;
             }
         }
         self.is_bingo = false;
-        false 
     }
 
-    pub fn score(&self, count: i32) -> i32 {
+    pub fn score(&self) -> i32 {
         let mut sum = 0;
         for row in &self.board {
             sum += row.into_iter().filter(|x| **x > 0).fold(0, |acc, x| acc + x);
         }
-        sum*count
+        println!("SUM {}", sum);
+        sum*self.win_val
     }
 }
 
@@ -112,26 +122,70 @@ impl Runnable for Day4 {
         let (v, mut bs) = self.read();
 
         let mut winning= 0;
-        let mut bingo = false;
-        println!("V: {:?}", v);
-        for i in v {
+        for (c, i) in v.into_iter().enumerate() {
             winning = i as i32;
             for b in bs.iter_mut() {
+                if b.is_bingo {
+                    b.win_turn = std::cmp::min(b.win_turn, c as u32);
+                    continue;
+                }
                 b.mark(i as i32);
                 b.check_bingo();
-                if b.is_bingo {
-                    bingo = true;
-                    break;
-                }
+                b.win_val = winning;
             }
-
-            if bingo {break;}
         }
 
-        for b in bs {
+        bs.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        for b in bs.into_iter().rev() {
             if b.is_bingo {
-                println!("Final Score: {}", b.score(winning));
+                println!("Final Score: {}, winning {}, order {}", b.score(), b.win_val, b.win_turn);
+                break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_row_bingo() {
+        let mut b = Board::new();
+
+        for _ in 0..4 {
+            b.add_row(vec![1; 5]);
+        }
+        b.add_row(vec![-1; 5]);
+        b.check_bingo();
+
+        assert!(b.is_bingo);
+    }
+
+    #[test]
+    fn test_col_bingo() {
+        let mut b = Board::new();
+
+        let row = vec![-1, 1, 1, 1, 1];
+        for _ in 0..5 {
+            b.add_row(row.clone());
+        }
+        b.check_bingo();
+
+        assert!(b.is_bingo);
+    }
+
+    #[test]
+    fn test_mark() {
+        let mut b = Board::new();
+
+        let row = vec![1, 2, 3, 4, 5];
+        for i in 1..6 {
+            b.add_row((&row).into_iter().map(|x| i*x).collect());
+        }
+
+        b.mark(1);
+
+        assert_eq!(b.board[0][0], -1);
     }
 }
